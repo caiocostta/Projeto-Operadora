aed4fe9c41d4463f3f7f1db0bd3d85990eefbe9c
caiocostta <caio.08costa@gmail.com>
2022-06-23 22:09:21 -0300

    Seção 2: Configurando Metodo Get

---
+++ Back/src/Operadoras.API/Controllers/OperadorasController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using Operadoras.API.Models;
+
+namespace Operadoras.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OperadorasController : ControllerBase
+    {
+
+        public IEnumerable<Operadora> _operadora = new Operadora[]{
+            new Operadora(){
+                OperadoraId = 1,
+                NomeOperadora = "America Net",
+                NumTelefone = 1135001000
+            },
+            new Operadora(){
+                OperadoraId = 2,
+                NomeOperadora = "Algar Telecom",
+                NumTelefone = 08009421212
+            }
+        };
+
+        public OperadorasController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Operadora> Get()
+        {
+            return _operadora;
+        }
+
+        [HttpGet ("{id}")]
+        public IEnumerable<Operadora> Get(int id)
+        {
+            return _operadora.Where(operadora => operadora.OperadoraId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de Put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo de Delete com id = {id}";
+        }
+    }
+}
---
+++ Back/src/Operadoras.API/Models/Operadora.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Operadoras.API.Models
+{
+    public class Operadora
+    {
+        public int OperadoraId { get; set; }
+
+        public string NomeOperadora { get; set; }
+
+        public long NumTelefone { get; set; }
+    }
+}
